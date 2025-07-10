ea4c91e50605010df2bb13921e78a47247eb1f90
Fajar Zuhri Hadiyanto <fajarzuhri76@gmail.com>
2025-07-10 13:52:26 +0700

    feat: clean main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
 mod macros;
 mod api;
-use api::v1::auth::models::user::User;
-mod lib;
-use lib::mongodb::{insert, find_all};
 
 #[tokio::main]
 async fn main() {
     let crate_name = env!("CARGO_PKG_NAME");
     println!("I am in crate `{}`", crate_name);
-
-    // Using the User struct from the macros module
-    let user = User::new(
-        "John Doe".to_string(),
-        "john.doe@example.com".to_string(),
-        "password".to_string(),
-        20
-    );
-    println!("User: {:?}", user);
-
-    // insert("users".into(), user).await;
-    find_all::<User>("users".into()).await;
 }
