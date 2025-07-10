d469f475b377d4211666c6488b4df561b599d40e
Fajar Zuhri Hadiyanto <fajarzuhri76@gmail.com>
2025-07-10 14:08:39 +0700

    docs: documenting macro rules generate_mongodb_object

--- src/macros/mongodb_object.rs
+++ src/macros/mongodb_object.rs
@@ -2,6 +2,10 @@
 #[macro_export]
 macro_rules! generate_mongodb_object {
     ($name:ident {$($field:ident: $type:ty),*}) => {
+        /// Generate a struct with a given fields and default fields which are:
+        /// - _id: mongodb object id
+        /// - created_at: date time of the creation of the object
+        /// - updated_at: date time of the last update of the object
         #[derive(Debug, serde::Serialize, serde::Deserialize)]
         pub struct $name {
             pub _id: mongodb::bson::oid::ObjectId,
@@ -11,6 +15,8 @@ macro_rules! generate_mongodb_object {
         }
 
         impl $name {
+            /// This function is used to create a new object with the given fields,
+            /// and fill the new _id, created_at, and updated_at
             pub fn new($($field: $type),*) -> Self {
                 Self {
                     _id: mongodb::bson::oid::ObjectId::new(),
@@ -20,6 +26,8 @@ macro_rules! generate_mongodb_object {
                 }
             }
 
+            /// This function is used to update the object with the given fields,
+            /// and update the updated_at field
             pub fn update(&mut self, $(#[allow(unused_variables)] $field: Option<$type>),*) {
                 self.updated_at = mongodb::bson::DateTime::now();
                 $(
