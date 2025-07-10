37f0eca939d587eb44321124f49ac18b532a8f76
Fajar Zuhri Hadiyanto <fajarzuhri76@gmail.com>
2025-07-10 14:10:21 +0700

    docs: documenting mongodb library

--- src/lib/mongodb.rs
+++ src/lib/mongodb.rs
@@ -2,12 +2,14 @@ use mongodb::{options::ClientOptions, Client, Collection, bson::Document, Databa
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
+/// This function is used to get the client of the mongodb
 pub async fn get_client() -> Client {
     let mut client_options = ClientOptions::parse("mongodb://quantum:oiZZ8pRQPaGKkidAxnv0@192.168.166.6:27017/?replicaSet=rs0").await.unwrap();
     client_options.app_name = Some("RustMongoCRUD".to_string());
     Client::with_options(client_options).unwrap()
 }
 
+/// This function is used to get the database of the mongodb
 pub async fn get_db(client: Option<Client>) -> Database {
     let c = match client {
         Some(c) => c,
@@ -16,6 +18,7 @@ pub async fn get_db(client: Option<Client>) -> Database {
     c.database("rust_playground")
 }
 
+/// This function is used to get the session of the mongodb
 pub async fn get_session(client: Option<Client>) -> ClientSession {
     let c = match client {
         Some(c) => c,
@@ -24,6 +27,7 @@ pub async fn get_session(client: Option<Client>) -> ClientSession {
     c.start_session().await.unwrap()
 }
 
+/// This function is used to insert a new object into the mongodb
 pub async fn insert<T: Serialize + Send + Sync>(collection_name: String, data: T) {
     let client = get_client().await;
     let db = get_db(Some(client)).await;
@@ -33,6 +37,7 @@ pub async fn insert<T: Serialize + Send + Sync>(collection_name: String, data: T
     println!("Result: {:?}", result);
 }
 
+/// This function is used to find all the objects in the mongodb
 pub async fn find_all<T: Serialize + for<'de> Deserialize<'de> + Debug + Send + Sync>(collection_name: String) {
     
     let client = get_client().await;
