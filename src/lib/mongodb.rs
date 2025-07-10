0a4dfdbf0effd5734648939ab1907922918ab5f5
Fajar Zuhri Hadiyanto <fajarzuhri76@gmail.com>
2025-07-10 13:52:14 +0700

    feat: add function to get client, session, and db instead of direct collection

--- src/lib/mongodb.rs
+++ src/lib/mongodb.rs
@@ -1,24 +1,43 @@
-use mongodb::{options::ClientOptions, Client, Collection, bson::Document};
+use mongodb::{options::ClientOptions, Client, Collection, bson::Document, Database, ClientSession};
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
-pub async fn get_collection<T: Serialize + Send + Sync>(collection_name: String) -> Collection<T> {
+pub async fn get_client() -> Client {
     let mut client_options = ClientOptions::parse("mongodb://quantum:oiZZ8pRQPaGKkidAxnv0@192.168.166.6:27017/?replicaSet=rs0").await.unwrap();
     client_options.app_name = Some("RustMongoCRUD".to_string());
-    let client = Client::with_options(client_options).unwrap();
-    let db = client.database("rust_playground");
-    db.collection(collection_name.as_str())
+    Client::with_options(client_options).unwrap()
+}
+
+pub async fn get_db(client: Option<Client>) -> Database {
+    let c = match client {
+        Some(c) => c,
+        None => get_client().await
+    };
+    c.database("rust_playground")
+}
+
+pub async fn get_session(client: Option<Client>) -> ClientSession {
+    let c = match client {
+        Some(c) => c,
+        None => get_client().await
+    };
+    c.start_session().await.unwrap()
 }
 
 pub async fn insert<T: Serialize + Send + Sync>(collection_name: String, data: T) {
-    let collection: Collection<T> = get_collection(collection_name).await;
+    let client = get_client().await;
+    let db = get_db(Some(client)).await;
+    let collection: Collection<T> = db.collection(collection_name.as_str());
 
     let result = collection.insert_one(data).await.unwrap();
     println!("Result: {:?}", result);
 }
 
 pub async fn find_all<T: Serialize + for<'de> Deserialize<'de> + Debug + Send + Sync>(collection_name: String) {
-    let collection: Collection<T> = get_collection(collection_name).await;
+    
+    let client = get_client().await;
+    let db = get_db(Some(client)).await;
+    let collection: Collection<T> = db.collection(collection_name.as_str());
 
     let result = collection.find(Document::new()).await;
     result.iter().for_each(|result| {
